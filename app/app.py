fe8a8667dd1902cea13422b53b5ea34fffa321fd
qikang <qikang@ctfo.com>
2020-08-19 09:46:12 +0800

    add app.py file

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import socket,os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index_page():
+    hostname = socket.gethostname()
+
+    env = os.environ
+    env_str =  str([ "{}: {} </br>".format(i,env[i]) for i in env ])
+
+    re = "主机名: {}</br>环境变量: {}".format(hostname,env_str)
+
+    return re
+
+if __name__ == '__main__':
+
+    app.debug = False
+    app.run(host='0.0.0.0',port=80)
